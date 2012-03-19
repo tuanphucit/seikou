554c52cc0633f2beb72dfc366ca480c13e3406f7
luckymancvp <luckymancvp@c4340393-464e-1ddc-9603-ad6682e16011>
2012-03-19 04:12:51 +0000

    Add csdl vs them view product

--- trunk/themes/shop/views/product/view.php
+++ trunk/themes/shop/views/product/view.php
@@ -83,21 +83,33 @@
 		</div>
 		
 		<div class="productdata">
-			<div class="infospan">Model <span>PT - 3</span></div>
-			<div class="infospan">Item no <span>2522</span></div>
-			<div class="infospan">Manufacturer <span>Nikon</span></div>			
+			<div class="infospan">
+				<?php echo $product->getAttributeLabel('id')?>
+				<span><?php echo $product->id?></span>
+			</div>
+			<div class="infospan">
+				<?php echo $product->getAttributeLabel('price')?>
+				<span><?php echo $product->price?></span>
+			</div>
+			<div class="infospan">
+				<?php echo $product->getAttributeLabel('type')?>
+				<span><?php echo $product->getProductType()?></span>
+			</div>
+			<div class="infospan">
+				<?php echo $product->getAttributeLabel('option')?>
+				<span><?php echo $product->option?></span>
+			</div>
 		</div>
 		
 		<div class="tabs">
 			<!-- tabs -->
 			<ul class="tabNavigation">
-				<li><a href="#desc">Description</a></li>
+				<li><a href="#desc"><?php echo Yii::t('user','Description')?></a></li>
 			</ul>
 			
 			<!-- tab containers -->
 			<div id="desc">
-		    	<p>Lorem ipsum dolor sit amet.</p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		    	<?php echo $product->description?>
 		  	</div>
 		</div>
 		
