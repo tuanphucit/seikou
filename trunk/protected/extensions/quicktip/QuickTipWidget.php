09350c4717e79915c8678fae858f10eb1d1f0721
luckymancvp <luckymancvp@c4340393-464e-1ddc-9603-ad6682e16011>
2012-05-14 16:36:35 +0000

    bo sym

--- trunk/protected/extensions/quicktip/QuickTipWidget.php
+++ trunk/protected/extensions/quicktip/QuickTipWidget.php
@@ -5,7 +5,7 @@ class QuickTipWidget extends CWidget {
 	
 	public function init(){
 		Yii::app()->getAssetManager()->linkAssets = true;
-		$assetsUrl = publish(dirname(__FILE__) . '/assets', false, -1, YII_DEBUG);
+		$assetsUrl = publish(dirname(__FILE__) . '/assets');
 		$cs = Yii::app()->clientScript;
 		$cs->registerScriptFile($assetsUrl . "/js/quicktip.js");
 		$cs->registerCssFile($assetsUrl . "/css/quicktip.css");
